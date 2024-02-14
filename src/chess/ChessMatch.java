da417d005ea72fc8b30e473468c2a6eaab71f3b6
Vinicius Andrade <andrade4605133@gmail.com>
2024-02-14 12:20:13 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -33,8 +33,8 @@ public class ChessMatch {
 
 	public void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(2, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(5, 4));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 
 	}
 }
